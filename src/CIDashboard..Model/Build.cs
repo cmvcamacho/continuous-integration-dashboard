c79640b0cb3db96ac942b2858598d016b59d847c
Carlos Camacho <cmvcamacho@gmail.com>
2015-03-08 21:30:40 +0000

    add draft to Build class

---
+++ src/CIDashboard..Model/Build.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIDashboard.Model
+{
+    public class Build
+    {
+        public string BuildId { get; set; }
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public string Status { get; set; }
+        public string Url { get; set; }
+        public bool Success { get; set; }
+
+        public int NumberTestPassed { get; set; } //PassedTestCount
+        public int NumberTestIgnored { get; set; } //IgnoredTestCount
+        public int NumberTestFailed { get; set; } //FailedTestCount
+        public int NumberTestTotal
+        {
+            get { return this.NumberTestPassed + this.NumberTestFailed + this.NumberTestIgnored; }
+        }
+
+        public int NumberStatementsCovered { get; set; } //CodeCoverageAbsSCovered
+        public int NumberStatementsTotal { get; set; } //CodeCoverageAbsSTotal
+        public double CodeCoverage
+        {
+            get
+            {
+                return this.NumberStatementsTotal == 0
+                    ? 0
+                    : Math.Round(
+                        (this.NumberStatementsCovered/(double) this.NumberStatementsTotal)*100, 2);
+            }
+        }
+
+    }
+}
